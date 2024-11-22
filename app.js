b046258c4b94d360250dc88418a5686cb21046af
sadir5ek <abdiraimovsadirbek@gmail.com>
2024-11-22 21:29:54 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const text = [
+ "Баткен",   
+"Иним,",
+"Абдираимов ",
+"дунуйого",
+"Умарбекович",
+"келгем.",
+". Атам,",
+{year:2005},
+"Апам,",
+"жарык",
+"Областынын",
+"Байкем,",
+"Биздин",
+"уй булоо",
+"караштуу",
+"Эжем,",
+{ namber: 6},
+"Бужун",
+"мучолорунун саны",
+"жана",
+"Мен",
+"жылы",
+"Районуна",
+ "Садирбек",   
+"айылында",
+
+];
+console.log(text[20],text[2],text[23],text[4],text[0],text[10],text[0],
+    text[22],text[14],text[17],text[24],text[7].year, text[9],text[3],text[5],
+    text[12],text[13],text[18],text[16].namber,text[6],text[8],text[15],
+    text[11],text[1],text[19],text[20],
+    
+);
+
+const obj = {
+    name:["Абдираимов","Умарбекович","Садирбек",],
+    year:2005,
+    namber:6,
+    names:[". Атам,","Байкем,","Иним","Апам,","Эжем,","Мен",],
+    location:["Баткен","Бужун","Областынын","Районуна","айылында",],
+    worlds :["дунуйого","келгем .","жарык","Биздин","караштуу","уй булоо",
+        "мучолорунун саны","жана","жылы"],
+}
+
+console.log(obj.names[5],obj.name[0],obj.name[2],obj.name[1],obj.location[0],obj.location[2],obj.location[0],obj.location[3],obj.worlds[4],obj.location[1],
+    obj.location[4],obj.year,obj.worlds[8],obj.worlds[2],obj.worlds[0],obj.worlds[1],obj.worlds[3],obj.worlds[5],obj.worlds[6],obj.namber,obj.names[0],
+    obj.names[3],obj.names[4],obj.names[1],obj.names[2], obj.worlds[7],obj.names[5],
+);
